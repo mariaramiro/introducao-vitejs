ef7f087d74b42ffb62c1ac2e0ae18f66f4cce6ac
mariaramiro <wellykasilvar@gmail.com>
2026-03-06 14:43:03 -0300

    Aula 06/03

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,10 @@
+function Exemplo01 (){
+    return (
+        <>
+            <h1>Exemplo 01</h1>
+            <p>Conteudo minimo para componente React</p>
+        </>
+    )
+}
+
+export default Exemplo01;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+
+function Home (){
+return (
+    <div> 
+        <h1>Aula de React com Vite JS</h1>
+    </div>
+)
+}
+
+export default Home;
