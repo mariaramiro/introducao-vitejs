94ccd34370ec14ff768709c34106aa442acc1a4a
mariaramiro <wellykasilvar@gmail.com>
2026-03-13 14:40:55 -0300

    aula 13/03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,21 @@
 
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router";
+
 function Home (){
+
+const Navigate = useNavigate();
+
 return (
     <div> 
         <h1>Aula de React com Vite JS</h1>
+
+        <button
+            onClick={() =>Navigate('/exemplo/1')}
+            >
+                Exemplo 1
+        </button>
+
     </div>
 )
 }
