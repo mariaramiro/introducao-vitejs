24d51e46f4660cd15af320f97063e8ff077099ae
mariaramiro <wellykasilvar@gmail.com>
2026-03-20 14:38:13 -0300

    aula20-03

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/home'
+
 import Exemplo01 from './pages/exemplos/ex-01';
 import Exemplo02 from './pages/exemplos/ex-02';
 
-function App(){
-  return(
+
+import Atividade1 from './pages/atividades/atv-01';
+
+function App() {
+  return (
     <Routes>
-      <Route  path="/" element={<Home />}/>
+      <Route path="/" element={<Home />} />
       <Route path="/exemplo/1" element={<Exemplo01 />} />
-       <Route path="/exemplo/2" element={<Exemplo02 />} />
+      <Route path="/exemplo/2" element={<Exemplo02 />} />
     </Routes>
   )
 }
