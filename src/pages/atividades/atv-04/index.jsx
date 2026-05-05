cb569637f0b088d6be4ace110fb314b84a4dd678
“mariaramiro <wellykasilvar@gmail.com>
2026-05-05 11:42:44 -0300

    05/05

---
+++ src/pages/atividades/atv-04/index.jsx
@@ -0,0 +1,63 @@
+
+import { useState } from 'react';
+import styles from './index.module.css';
+
+export default function Atividade04() {
+    const [produto, setProduto] = useState('');
+    const [quantidade, setQuantidade] = useState('');
+    const [lista, setLista] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!produto || !quantidade) return;
+
+        const novoItem = {
+            produto,
+            quantidade
+        };
+
+        setLista([...lista, novoItem]);
+
+        setProduto('');
+        setQuantidade('');
+    };
+
+    return (
+        <div className={styles.container}>
+
+            <h1>Lista de Compras</h1>
+
+            <form onSubmit={handleSubmit} className={styles.formulario}>
+                
+                <input
+                    type="text"
+                    placeholder='Nome do produto'
+                    value={produto}
+                    onChange={e => setProduto(e.target.value)}
+                    className={styles.produto}
+                />
+
+                <input
+                    type="number"
+                    placeholder='Qtd'
+                    value={quantidade}
+                    onChange={e => setQuantidade(e.target.value)}
+                    className={styles.quantidade}
+                />
+
+                <button type="submit">Adicionar</button>
+            </form>
+
+            <h2>Lista</h2>
+
+            {
+                lista.map((item, index) => (
+                    <p key={index}>
+                        {item.produto} - {item.quantidade}
+                    </p>
+                ))
+            }
+        </div>
+    );
+}
